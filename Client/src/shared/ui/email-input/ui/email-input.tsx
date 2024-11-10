e6393122721f5eccf08bc434fb800d979489139d
Edanriell <Edanriell@gmail.com>
2024-11-10 11:35:00 +0200

    feature(config): configured app

---
+++ Client/src/shared/ui/email-input/ui/email-input.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export const EmailInput: FC = () => {
+	return null;
+};
--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,7 +1,26 @@
+import { fileURLToPath, URL } from "node:url";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [
+		TanStackRouterVite({
+			routesDirectory: "./src/app/routes",
+			generatedRouteTree: "./src/app/routeTree.gen.ts"
+		}),
+		react()
+	],
+	resolve: {
+		alias: [
+			{ find: "@app", replacement: fileURLToPath(new URL("./src/app", import.meta.url)) },
+			{ find: "@entities", replacement: fileURLToPath(new URL("./src/entities", import.meta.url)) },
+			{ find: "@features", replacement: fileURLToPath(new URL("./src/features", import.meta.url)) },
+			{ find: "@pages", replacement: fileURLToPath(new URL("./src/pages", import.meta.url)) },
+			{ find: "@shared", replacement: fileURLToPath(new URL("./src/shared", import.meta.url)) },
+			{ find: "@widgets", replacement: fileURLToPath(new URL("./src/widgets", import.meta.url)) }
+		]
+	}
 });

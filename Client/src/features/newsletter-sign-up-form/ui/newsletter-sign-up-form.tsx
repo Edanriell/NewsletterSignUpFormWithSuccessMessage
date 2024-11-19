7a80372f59618e0e1d0291b2953edd447f8131c8
Edanriell <Edanriell@gmail.com>
2024-11-19 12:25:51 +0200

    feature(success-ui-animations): created animations, but optimization is needed

--- Client/src/features/newsletter-sign-up-form/ui/newsletter-sign-up-form.tsx
+++ Client/src/features/newsletter-sign-up-form/ui/newsletter-sign-up-form.tsx
@@ -9,24 +9,6 @@ import { useWindowSize } from "@shared/lib/hooks";
 import { createNewsletter } from "../api";
 import { extractErrorMessage } from "../lib/functions";
 import { SignUpFormSchema } from "../model";
-
-import {
-	NewsletterSignUpFormContent,
-	NewsletterSignUpFormImage,
-	NewsletterSignUpFormSubscriptionBenefitsList,
-	NewsletterSignUpFormSubscriptionBenefitsListItem,
-	NewsletterSignUpFormText,
-	NewsletterSignUpFormTitle,
-	SignUpFormField,
-	SignUpFormFieldset,
-	SignUpFormLabel,
-	SignUpFormLegend,
-	StyledNewsletterSignUpForm,
-	StyledNewsletterSignUpFormSubscriptionSuccessMessage,
-	StyledNewsletterSignUpFormSubscriptionSuccessMessageContent,
-	StyledNewsletterSignUpFormSubscriptionSuccessMessageText,
-	StyledNewsletterSignUpFormSubscriptionSuccessMessageTitle
-} from "./styles";
 import styles from "./styles.module.less";
 
 type SignUpFormValues = {
@@ -39,7 +21,8 @@ const NewsletterSignUpFormSvgImage: FC = () => {
 	return (
 		<Fragment>
 			{width && width >= 1440 ? (
-				<svg
+				<motion.svg
+					layout
 					style={{ border: "none", width: "100%", height: "100%", overflow: "hidden" }}
 					width="400"
 					shapeRendering="geometricPrecision"
@@ -375,9 +358,10 @@ const NewsletterSignUpFormSvgImage: FC = () => {
 							<stop offset="1" stopColor="#FF5B66" />
 						</linearGradient>
 					</defs>
-				</svg>
+				</motion.svg>
 			) : (
-				<svg
+				<motion.svg
+					layout
 					style={{ border: "none", width: "100%", overflow: "hidden" }}
 					preserveAspectRatio="xMaxYMid slice"
 					shapeRendering="geometricPrecision"
@@ -725,7 +709,7 @@ const NewsletterSignUpFormSvgImage: FC = () => {
 							<stop offset="1" stopColor="#FF5B66" />
 						</linearGradient>
 					</defs>
-				</svg>
+				</motion.svg>
 			)}
 		</Fragment>
 	);
@@ -733,7 +717,8 @@ const NewsletterSignUpFormSvgImage: FC = () => {
 
 const NewsletterSignUpFormSubscriptionBenefitsListCheckmarkIcon: FC = () => {
 	return (
-		<svg
+		<motion.svg
+			layout
 			style={{ flex: "0 0 auto" }}
 			width="21"
 			height="21"
@@ -743,13 +728,14 @@ const NewsletterSignUpFormSubscriptionBenefitsListCheckmarkIcon: FC = () => {
 		>
 			<circle cx="10.5" cy="10.5" r="10.5" fill="#FF6155" />
 			<path d="M6 11.3812L8.73464 14L15 8" stroke="white" strokeWidth="2" />
-		</svg>
+		</motion.svg>
 	);
 };
 
 const NewsletterSignUpFormSubscriptionSuccessIcon: FC = () => {
 	return (
-		<svg
+		<motion.svg
+			layout
 			style={{ flex: "0 0 auto" }}
 			shapeRendering="geometricPrecision"
 			width="64"
@@ -773,7 +759,7 @@ const NewsletterSignUpFormSubscriptionSuccessIcon: FC = () => {
 					<stop offset="1" stopColor="#FF527B" />
 				</linearGradient>
 			</defs>
-		</svg>
+		</motion.svg>
 	);
 };
 
@@ -820,101 +806,154 @@ export const NewsletterSignUpForm: FC = () => {
 		}
 	};
 
+	const handleDismissMessageButtonClick = () => {
+		setSignUpFormState("idle");
+	};
+
 	return (
 		<Fragment>
-			<StyledNewsletterSignUpForm>
-				<NewsletterSignUpFormImage>
-					<NewsletterSignUpFormSvgImage />
-				</NewsletterSignUpFormImage>
-				<NewsletterSignUpFormContent>
-					<NewsletterSignUpFormTitle>Stay updated!</NewsletterSignUpFormTitle>
-					<NewsletterSignUpFormText>
-						Join 60,000+ product managers receiving monthly updates on:
-					</NewsletterSignUpFormText>
-					<NewsletterSignUpFormSubscriptionBenefitsList>
-						<NewsletterSignUpFormSubscriptionBenefitsListItem>
-							<NewsletterSignUpFormSubscriptionBenefitsListCheckmarkIcon />
-							Product discovery and building what matters
-						</NewsletterSignUpFormSubscriptionBenefitsListItem>
-						<NewsletterSignUpFormSubscriptionBenefitsListItem>
-							<NewsletterSignUpFormSubscriptionBenefitsListCheckmarkIcon />
-							Measuring to ensure updates are a success
-						</NewsletterSignUpFormSubscriptionBenefitsListItem>
-						<NewsletterSignUpFormSubscriptionBenefitsListItem>
-							<NewsletterSignUpFormSubscriptionBenefitsListCheckmarkIcon />
-							And much more!
-						</NewsletterSignUpFormSubscriptionBenefitsListItem>
-					</NewsletterSignUpFormSubscriptionBenefitsList>
-					<Formik
-						initialValues={initialSignUpFormValues}
-						onSubmit={handleSignUpFormSubmit}
-						validationSchema={SignUpFormSchema}
-					>
-						{({ errors, touched, isSubmitting }) => (
-							<Form>
-								<SignUpFormFieldset>
-									<SignUpFormLegend>Subscribe to the Newsletter</SignUpFormLegend>
-									<SignUpFormField>
-										<SignUpFormLabel htmlFor="emailAddress">Email address</SignUpFormLabel>
-										<Input
-											id="emailAddress"
-											name="emailAddress"
-											type="email"
-											placeholder="email@company.com"
-											isInputValid={!(errors.emailAddress && touched.emailAddress)}
-										/>
-										<AnimatePresence>
-											{errors.emailAddress && touched.emailAddress && (
-												<motion.p
-													initial={{ opacity: 0, x: -10 }}
-													animate={{ opacity: 1, x: 0 }}
-													exit={{ opacity: 0, x: -10 }}
-													transition={{ type: "spring", duration: 0.3, bounce: 0 }}
-													className={styles["error-message"]}
-												>
-													{errors.emailAddress}
-												</motion.p>
-											)}
-											{!(errors.emailAddress && touched.emailAddress) &&
-												signUpFormState === "unsubmitted" && (
+			{signUpFormState === "submitted" ? (
+				<motion.article
+					layout
+					layoutId="newsletter-sign-up-form"
+					className={styles["newsletter-sign-up-form-subscription-success-message"]}
+				>
+					<motion.div layout>
+						<NewsletterSignUpFormSubscriptionSuccessIcon />
+						<motion.h2
+							layout
+							className={styles["newsletter-sign-up-form-subscription-success-message__title"]}
+						>
+							Thanks for subscribing!
+						</motion.h2>
+						<motion.p
+							layout
+							className={styles["newsletter-sign-up-form-subscription-success-message__text"]}
+						>
+							A confirmation email has been sent to <strong>ash@loremcompany.com</strong>. Please
+							open it and click the button inside to confirm your subscription
+						</motion.p>
+					</motion.div>
+					<Button type="button" onClick={handleDismissMessageButtonClick}>
+						Dismiss message
+					</Button>
+				</motion.article>
+			) : (
+				<motion.article
+					layout
+					layoutId="newsletter-sign-up-form"
+					className={styles["newsletter-sign-up-form"]}
+				>
+					<motion.div layout className={styles["newsletter-sign-up-form__image-wrapper"]}>
+						<NewsletterSignUpFormSvgImage />
+					</motion.div>
+					<motion.div layout className={styles["newsletter-sign-up-form__content"]}>
+						<motion.h2 layout className={styles["newsletter-sign-up-form__title"]}>
+							Stay updated!
+						</motion.h2>
+						<motion.p layout className={styles["newsletter-sign-up-form__text"]}>
+							Join 60,000+ product managers receiving monthly updates on:
+						</motion.p>
+						<motion.ul
+							layout
+							className={styles["newsletter-sign-up-form__subscription-benefits-list"]}
+						>
+							<motion.li
+								layout
+								className={styles["newsletter-sign-up-form__subscription-benefits-list-item"]}
+							>
+								<NewsletterSignUpFormSubscriptionBenefitsListCheckmarkIcon />
+								Product discovery and building what matters
+							</motion.li>
+							<motion.li
+								layout
+								className={styles["newsletter-sign-up-form__subscription-benefits-list-item"]}
+							>
+								<NewsletterSignUpFormSubscriptionBenefitsListCheckmarkIcon />
+								Measuring to ensure updates are a success
+							</motion.li>
+							<motion.li
+								layout
+								className={styles["newsletter-sign-up-form__subscription-benefits-list-item"]}
+							>
+								<NewsletterSignUpFormSubscriptionBenefitsListCheckmarkIcon />
+								And much more!
+							</motion.li>
+						</motion.ul>
+						<Formik
+							initialValues={initialSignUpFormValues}
+							onSubmit={handleSignUpFormSubmit}
+							validationSchema={SignUpFormSchema}
+						>
+							{({ errors, touched, isSubmitting }) => (
+								<Form>
+									<motion.fieldset layout className={styles["sign-up-form__fieldset"]}>
+										<motion.legend layout className={styles["sign-up-form__legend"]}>
+											Subscribe to the Newsletter
+										</motion.legend>
+										<motion.div layout className={styles["sign-up-form__field"]}>
+											<motion.label
+												layout
+												className={styles["sign-up-form__label"]}
+												htmlFor="emailAddress"
+											>
+												Email address
+											</motion.label>
+											<Input
+												id="emailAddress"
+												name="emailAddress"
+												type="email"
+												placeholder="email@company.com"
+												isInputValid={!(errors.emailAddress && touched.emailAddress)}
+											/>
+											<AnimatePresence>
+												{errors.emailAddress && touched.emailAddress && (
 													<motion.p
 														initial={{ opacity: 0, x: -10 }}
 														animate={{ opacity: 1, x: 0 }}
 														exit={{ opacity: 0, x: -10 }}
-														transition={{ type: "spring", duration: 0.3, bounce: 0 }}
+														transition={{
+															type: "spring",
+															duration: 0.3,
+															bounce: 0
+														}}
 														className={styles["error-message"]}
 													>
-														{errorMessage}
+														{errors.emailAddress}
 													</motion.p>
 												)}
-										</AnimatePresence>
-									</SignUpFormField>
-									<Button
-										type="submit"
-										disabled={isSubmitting || signUpFormState === "unsubmitted"}
-										state={buttonStateMap[signUpFormState]}
-									>
-										Subscribe to monthly newsletter
-									</Button>
-								</SignUpFormFieldset>
-							</Form>
-						)}
-					</Formik>
-				</NewsletterSignUpFormContent>
-			</StyledNewsletterSignUpForm>
-			<StyledNewsletterSignUpFormSubscriptionSuccessMessage>
-				<StyledNewsletterSignUpFormSubscriptionSuccessMessageContent>
-					<NewsletterSignUpFormSubscriptionSuccessIcon />
-					<StyledNewsletterSignUpFormSubscriptionSuccessMessageTitle>
-						Thanks for subscribing!
-					</StyledNewsletterSignUpFormSubscriptionSuccessMessageTitle>
-					<StyledNewsletterSignUpFormSubscriptionSuccessMessageText>
-						A confirmation email has been sent to <strong>ash@loremcompany.com</strong>. Please open
-						it and click the button inside to confirm your subscription
-					</StyledNewsletterSignUpFormSubscriptionSuccessMessageText>
-				</StyledNewsletterSignUpFormSubscriptionSuccessMessageContent>
-				<Button type="button">Dismiss message</Button>
-			</StyledNewsletterSignUpFormSubscriptionSuccessMessage>
+												{!(errors.emailAddress && touched.emailAddress) &&
+													signUpFormState === "unsubmitted" && (
+														<motion.p
+															initial={{ opacity: 0, x: -10 }}
+															animate={{ opacity: 1, x: 0 }}
+															exit={{ opacity: 0, x: -10 }}
+															transition={{
+																type: "spring",
+																duration: 0.3,
+																bounce: 0
+															}}
+															className={styles["error-message"]}
+														>
+															{errorMessage}
+														</motion.p>
+													)}
+											</AnimatePresence>
+										</motion.div>
+										<Button
+											type="submit"
+											disabled={isSubmitting || signUpFormState === "unsubmitted"}
+											state={buttonStateMap[signUpFormState]}
+										>
+											Subscribe to monthly newsletter
+										</Button>
+									</motion.fieldset>
+								</Form>
+							)}
+						</Formik>
+					</motion.div>
+				</motion.article>
+			)}
 		</Fragment>
 	);
 };
